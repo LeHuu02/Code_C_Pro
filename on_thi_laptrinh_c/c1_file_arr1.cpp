7606edf3fa5f264e2b7b6e38f1d1cb7f606b49dd
Le Minh Huu <cnleminhhuu.com>
2022-11-18 12:18:20 +0700

    update c pro dslk don so nguyen, doi tuong by minh huu

---
+++ on_thi_laptrinh_c/c1_file_arr1.cpp
@@ -0,0 +1,15 @@
+/*
+power by le minh huu 16/11/2022 @LeHuu02 @copyright
+yeu cau:
+ đọc ghi dữ liệu vào file.txt
+        - đọc mảng 1 chiều từ file.txt
+        - thực hiện tính tổng mảng vừa đọc
+        - thực hiện các phép toán trong mảng 1 chiều: số nt, cp, hh, xắp sếp, min max,...
+        - ghi kết quả vừa thực hiện được vào file_kq.txt
+*/
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
---
+++ on_thi_laptrinh_c/c1_pointer_arr1.cpp
@@ -0,0 +1,16 @@
+
+/*
+power by le minh huu 16/11/2022 @LeHuu02 @copyright
+yeu cau:
+    con trỏ với mảng 1 chiều 
+             - nhập n, nhập xuất mảng
+             - tìm và tính tổng số nguyên tố, chinh phương, hoàn hảo. 
+             - xắp sếp tăng dần giảm dần, tim min max. 
+             - tổng các số chia hết cho 2 3 5,... số 2 số có đơn vị = 7,...
+*/
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
